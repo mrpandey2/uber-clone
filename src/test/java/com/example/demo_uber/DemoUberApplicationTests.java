d3abe80f51bdb4628e6462ea4b738e3b56abca2c
prashu12 <pandeyprashantganesh@gmail.com>
2024-11-09 18:18:46 +0530

    Initial Commit

---
+++ src/test/java/com/example/demo_uber/DemoUberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
